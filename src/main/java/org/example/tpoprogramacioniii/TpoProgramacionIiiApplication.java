0b3baf8e6e60b7b29a03e34a747cbd9825d36fe4
Lucio <lclaa@uade.edu.ar>
2025-09-27 13:38:05 -0300

    ahora si anda la conexion, servidor tomcat añadido

--- src/main/java/org/example/tpoprogramacioniii/TpoProgramacionIiiApplication.java
+++ src/main/java/org/example/tpoprogramacioniii/TpoProgramacionIiiApplication.java
@@ -21,16 +21,7 @@ public class TpoProgramacionIiiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TpoProgramacionIiiApplication.class, args);
-
-        // URI examples: "neo4j://localhost", "neo4j+s://xxx.databases.neo4j.io"
-        final String dbUri = "neo4j+s://f140e33c.databases.neo4j.io";
-        final String dbUser = "neo4j";
-        final String dbPassword = "neo4jadmin";
-
-        try (var driver = GraphDatabase.driver(dbUri, AuthTokens.basic(dbUser, dbPassword))) {
-            driver.verifyConnectivity();
-            System.out.println("Connection established.");
-        }
+        System.out.println("arranca ");
     }
 
 }
